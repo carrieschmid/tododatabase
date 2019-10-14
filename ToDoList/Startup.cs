26ea26e198b99dfdbb8273c3d8c924e968046280
Student <example@example.com>
2019-10-14 16:45:51 -0700

    startup change

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -8,11 +8,7 @@ using ToDoList.Models;
 
 namespace ToDoList
 {
-   public static class DBConfiguration
-    {
-      public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
-    }
-    
+   
   public class Startup
   {
     public Startup(IHostingEnvironment env)
