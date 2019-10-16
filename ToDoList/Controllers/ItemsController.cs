1acdf47a083cf199e89681d89d29100f6da6d6dd
Student <example@example.com>
2019-10-16 09:51:32 -0700

    Add Tuesday's homework to project.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,41 +2,75 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller
-  {
-    private readonly ToDoListContext _db;
-
-    public ItemsController(ToDoListContext db)
+    public class ItemsController : Controller
     {
-      _db = db;
-    }
+        private readonly ToDoListContext _db;
 
-    public ActionResult Index()
-    {
-      List<Item> model = _db.Items.ToList();
-      return View(model);
-    }
+        public ItemsController(ToDoListContext db)
+        {
+            _db = db;
+        }
 
-    public ActionResult Create()
-    {
-        return View();
-    }
+        public ActionResult Index()
+        {
+            List<Item> model = _db.Items.Include(items => items.Category).ToList(); //We can utilize eager loading by using Entity's built-in Include() methodThis basically states the following: for each Item in the database, include the Category it belongs to and then put all the Items into list.
+            return View(model);
+        }
 
-    [HttpPost]
-    public ActionResult Create(Item item)
-    {
-        _db.Items.Add(item);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
 
-    public ActionResult Details(int id)
-    {
-        Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-        return View(thisItem);
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+            //^This is our way of communicating to the database, giving it the id that was passed in and telling the database to give us the item in the database that has this id. In this line, "items" is not a special name.
+            return View(thisItem);
+        }
+        public ActionResult Edit(int id)
+        {
+            var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+            ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+            _db.Items.Remove(thisItem);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
-  }
 }
