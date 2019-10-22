aa415302dd12bd0cfad1b0123abc9a80798685e3
Carrie Schmid <carriepederson7@gmail.com>
2019-10-21 22:04:40 -0700

    Add Tuesday homework.

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace BasicAuthentication.Models
 {
     public class ToDoListContext : IdentityDbContext<ApplicationUser>
